ccfd7c92a7f52f46dc7957179fcab096cfa67c76
Charles Hamilton <crhamiltonj@gmail.com>
2019-10-20 13:33:19 -0400

    correcting issues with static templates

--- web/app/landing/routes.py
+++ web/app/landing/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-landing_bp = Blueprint('landing_bp', __name__, template_folder='templates', static_folder='static')
+landing_bp = Blueprint('landing_bp', __name__, template_folder='templates', static_folder='static', static_url_path='/landing')
 
 @landing_bp.route("/")
 def index():
